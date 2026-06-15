f35789d4005f71a7263d5565a1eebc6f59234995
164379-pixel <164379@wsk.ac.th>
2026-06-15 02:44:04 +0000

    1.py

--- 1.py
+++ 1.py
@@ -1,1 +1,8 @@
-print()
+# Rectangle Area Calculator (User Input Version)
+
+width = float(input("Enter width: "))
+length = float(input("Enter length: "))
+
+area = width * length
+
+print("The area is:", area)
